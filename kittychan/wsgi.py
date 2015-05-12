f048255f9909ba5021a71973e4de2451adf2ca39
Jonan Scheffler <jonanscheffler@gmail.com>
2015-05-12 15:12:35 -0700

    User whitenoise for static assets.

--- kittychan/wsgi.py
+++ kittychan/wsgi.py
@@ -7,17 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+import os
 import newrelic.agent
 newrelic.agent.initialize('newrelic.ini')
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kittychan.settings")
 
+from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
 if os.environ.get('DYNO'):
-    application = Cling(get_wsgi_application())
+    application = Cling(get_wsgi_application(application))
 else:
-    application = get_wsgi_application()
+    application = DjangoWhiteNoise(application)
