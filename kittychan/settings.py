2bbdeb51f344033266656d7c04997c3b8a51b4fd
Jonan Scheffler <jonanscheffler@gmail.com>
2015-05-12 14:57:10 -0700

    Fix static root.

--- kittychan/settings.py
+++ kittychan/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
